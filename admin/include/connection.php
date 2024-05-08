86d88458584e99c715d5fe6f317c22b7d6f61b4d
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-05-08 21:07:55 +0500

    Initial Commit Login Created

---
+++ admin/include/connection.php
@@ -0,0 +1,19 @@
+<?php
+require('include/functions.php');
+
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "northwind";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
